d301c0ed9ef6b7b8b238e1fa77d121fa5a1231f5
jainatina <jainatina@gmail.com>
2017-06-26 19:55:34 -0400

    Prime Factors, 1, 2, 3

---
+++ PrimeFactors/PrimeFactors.Tests/PrimeFactorsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PrimeFactors.Tests
+{
+    [TestClass]
+    public class PrimeFactorsTests
+    {
+        private PrimeFact primeFactors;
+
+        [TestInitialize]
+        public void Setup()
+        {
+           primeFactors = new PrimeFact();
+        }
+
+        [TestMethod]
+        public void FactorsOfOne()
+        {
+            var factors = primeFactors.Factor(1);
+
+            Assert.IsTrue(!factors.Any());
+
+        }
+        [TestMethod]
+        public void FactorsOfTwo()
+        {
+            var expected = new List<int>() { 2 };
+
+            var factors = primeFactors.Factor(2);
+
+            CollectionAssert.AreEqual(expected, factors);
+
+        }
+        [TestMethod]
+        public void FactorsOfThree()
+        {
+            var expected = new List<int>() { 3 };
+
+            var factors = primeFactors.Factor(3);
+
+            CollectionAssert.AreEqual(expected, factors);
+
+        }
+
+    }
+}
---
+++ PrimeFactors/PrimeFactors/PrimeFact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class PrimeFact
+    {
+        public List<int> Factor(int num)
+        {
+            List<int> factList = new List<int>();
+
+            if (num < 2)
+            {
+                return factList;
+            }
+            if (num == 2)
+            {
+                factList.Add(2);
+            }
+            if (num == 3)
+            {
+                factList.Add(3);
+            }
+
+            return factList;
+        }
+    }
+}
